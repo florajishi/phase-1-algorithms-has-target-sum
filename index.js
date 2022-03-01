a4228b9d867ab9f3c9bad7edc5a80f082118ce64
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-03-01 15:11:06 -0600

    commit tree 2989d60f43c5dd8a6bc768cf01123ec207035133

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+let diff
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let obj = {}
+  for(let index = 0; index < array.length; index++){
+    diff = target - array[index]
+    if(obj[diff]){
+      return true
+    } else{
+      obj[array[index]] = true
+    }
+  }
+  return false
 }
-
+console.log(hasTargetSum([1,2,3], 5))
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+function hasTargetSum(array, target)
+  let empty Object function as hash table
+  for every index of the array starting at index 0, within the restrictions of the length of the array, we will loop through every index
+    variable diff is (target - array[index])
+    diff is put into object
+      if any key in the object (alternatively, element of the array) matches diff
+        return true
+        otherwise, false
+
 */
 
 /*
   Add written explanation of your solution here
+  
+  I declare the function hasTargetSum to take in the arguments array [] and an int called target
+    To improve the time complexity, we utilize the JavaScript Object as a Hash Table. a Hash Table lets us store a key to a specific value. We declare an empty object and input the keys later on in the function
+      we create a for loop that interates over each index within the length of the array starting at 0
+      the variable diff is declared, which the value being the target integer subtracted by the element(array integer) at the index of array
+      the diff variable is mapped to the empty object as a key
+        if any element in the array (key in object) matches the diff, return true
+        otherwise the program returns false
+
 */
 
 // You can run `node index.js` to view these console logs
